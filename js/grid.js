c1bc8baf3bfe68432e272f7fb0825082cd8ab15b
Ariel <ariburr@gmail.com>
2017-11-04 12:12:14 -0600

    first

---
+++ js/grid.js
@@ -0,0 +1,6 @@
+$("#sprite-button").on("click", function(){
+  $("#coffee-info").fadeIn("slow");
+  $("#film-info").delay(100).fadeIn("slow");
+  $("#climb-info").delay(200).fadeIn("slow");
+  $("#music-info").delay(300).fadeIn("slow");
+});
