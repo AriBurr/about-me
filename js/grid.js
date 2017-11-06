706dfa2f5ac4d4c5c1971556722758016e1f8275
Ariel <ariburr@gmail.com>
2017-11-05 18:22:33 -0700

    final

--- js/grid.js
+++ js/grid.js
@@ -4,3 +4,31 @@ $("#sprite-button").on("click", function(){
   $("#climb-info").delay(200).fadeIn("slow");
   $("#music-info").delay(300).fadeIn("slow");
 });
+
+$("#coffee-info").on("mouseover", function(){
+  $("#long").addClass("long-color");
+});
+
+$("#film-info").on("mouseover", function(){
+  $("#creative").addClass("creative-color");
+  $("#expressive").addClass("expressive-color");
+});
+
+$("#climb-info").on("mouseover", function(){
+  $("#challenge").addClass("challenge-color");
+});
+
+$("#music-info").on("mouseover", function(){
+  $("#unique").addClass("unique-color");
+  $("#interactive").addClass("interactive-color");
+});
+
+var container = document.querySelector(".grid-container-circle");
+var comp = document.querySelector("#sprite-comp")
+
+container.addEventListener("mouseover", function() {
+  comp.style.animation = ("sprite-4-steps-800 .5s steps(4) infinite");
+  container.addEventListener("mouseout", function() {
+    comp.style.animation = ("");
+  });
+});
